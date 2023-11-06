e583bf93466b131b3a146912d2b3d824726aff57
Stefano <stef.pro@hotmail.com>
2023-11-06 10:35:44 -0500

    chore: create frontend for login and signup

---
+++ public/form/signin-form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Sign In</title>
+    <link rel="stylesheet" href="../assets/css/signIn.css" />
+</head>
+
+<body>
+    <div class="main">
+        <div class="container_2">
+            <div class="promo-container">
+                <h2>LaSalle Quiz Game</h2>
+            </div>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <div class="button-container">
+                    <button>Login</button>
+                </div>
+                <p>Don't have an account? <a href="signup-form.php">Create One!</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ src/features/signin.php
@@ -0,0 +1,36 @@
+<?php
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Assignment 0</title>
+    <link rel="stylesheet" href="../styles/signin.css" />
+</head>
+
+<body>
+    <div class="main">
+        <div class="container_2">
+            <div class="promo-container">
+                <h2>LaSalle Quiz Game</h2>
+            </div>
+            <form>
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Last Name" />
+                <input type="email" placeholder="Email Address" />
+                <input type="password" placeholder="Password" />
+                <div class="button-container">
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
+
+
+?>
