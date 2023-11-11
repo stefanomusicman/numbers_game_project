adf0ef54c52c070d09ba007f6f903472bfc86a1b
Stefano <stef.pro@hotmail.com>
2023-11-10 21:35:03 -0500

    built frontend for pw-update page

---
+++ public/form/pw-update-form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Update Password</title>
+    <link rel="stylesheet" href="../assets/css/pw-update.css" />
+</head>
+
+<body>
+    <div class="main">
+        <nav>
+            <a><h3>Play Game</h3></a>
+            <a><h3>Update Password</h3></a>
+            <a><h3>Game History</h3></a>
+            <button>Logout</button>
+        </nav>
+        <div class="container_2">
+            <div class="promo-container">
+                <h2>Update Password</h2>
+            </div>
+            <form>
+                <input name="old-password" type="text" placeholder="Old Password" />
+                <input name="new-password" type="text" placeholder="New Password" />
+                <div class="button-container">
+                    <button>Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
