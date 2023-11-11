c799a271220c7657eecdcbdc4dcfc4103aa02ddc
Stefano <stef.pro@hotmail.com>
2023-11-10 22:40:03 -0500

    relocated the nav bar to make it reusable

--- public/form/pw-update-form.php
+++ public/form/pw-update-form.php
@@ -5,16 +5,14 @@
     <meta charset="UTF-8" />
     <title>Update Password</title>
     <link rel="stylesheet" href="../assets/css/pw-update.css" />
+    <link rel="stylesheet" href="../assets/css/style.css" />
 </head>
 
 <body>
     <div class="main">
-        <nav>
-            <a><h3>Play Game</h3></a>
-            <a><h3>Update Password</h3></a>
-            <a><h3>Game History</h3></a>
-            <a href="../../src/features/signout.php"><h3>Logout</h3></a>
-        </nav>
+
+        <?php include '../template/nav.php'; ?>
+        
         <div class="container_2">
             <div class="promo-container">
                 <h2>Update Password</h2>
