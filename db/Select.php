7fc7133a153d63ba6d8479e2fbb704e30553e17b
Carolina Ruiz <ing.acrd@gmail.com>
2023-11-23 15:48:29 -0500

    verify user and password

--- db/Select.php
+++ db/Select.php
@@ -3,30 +3,49 @@
 require_once "../../public/model/user.php";
 class Select extends Database {
 
-    private $tableName;
     private $user;
     private $password;
+    private $registrationOrder = 0;
 
     //Constructor method 
-    public function __construct($table, $user, $password ){
+    public function __construct($user, $password ){
 
-        $this->tableName = $table;
         $this->user = $user;
         $this->password = $password;
+        
     }
 
+    public function setRegistrationOrder() {
+        //Assign sql query
+        $sql = $this->sql();
+        //1-CONNECT TO MYSQL
+        $this->connectToMySQL(HOST, USER, PASS);
+        //2-SELECT THE DATABASE
+        $this->selectDatabase(DBASE);
+        //4-EXECUTE THE QUERY TO SELECT FROM THE TABLE
+       
+
+        $dataFound = $this->executeQuery($sql['selectUser']);
+
+        if (!empty($dataFound)) {
+        
+            if(count($dataFound) === 1){
+                $this->registrationOrder = $dataFound['row1']['registrationOrder'];
+            }
+        }
+    }
 
 
     //SQL query method
     private function sql(){
-        $sql['descTable'] = "DESC " . $this->tableName;
-        $sql['selectAllColumns'] = "SELECT * FROM " . $this->tableName;
-        $sql['selectUser'] = "SELECT * FROM " . $this->tableName . " WHERE userName = '" . $this->user . "'";
+        $sql['descTable'] = "DESC player";
+        $sql['selectUser'] = "SELECT * FROM player WHERE userName = '" . $this->user . "'";
+        $sql['selectAuth'] = "SELECT * FROM authenticator WHERE registrationOrder = '". $this->registrationOrder."'";
         return $sql;
     }
+    
 
-    public function checkUser(){
-
+    public function checkUserName(){
         //Assign sql query
         $sql = $this->sql();
         //1-CONNECT TO MYSQL
@@ -39,14 +58,36 @@ class Select extends Database {
         if (!empty($dataFound)) {
         
             if(count($dataFound) === 1){
-                $userLogged = new User($dataFound['row1']['userName']);
-                
-                return $userLogged;
-            } 
+                $this->registrationOrder = $dataFound['row1']['registrationOrder'];
+                return true;
+            }
+            return false;
         } else {
-            return $userNotFound = new User("");
+            return false;
         }
             
+    }
+
+    public function checkPassword() {
+        //Assign sql query
+        $sql = $this->sql();
+        //1-CONNECT TO MYSQL
+        $this->connectToMySQL(HOST, USER, PASS);
+        //2-SELECT THE DATABASE
+        $this->selectDatabase(DBASE);
+        //4-EXECUTE THE QUERY TO SELECT FROM THE TABLE
+        $dataFound = $this->executeQuery($sql['selectAuth']);
+        if (!empty($dataFound)) {
+            if(count($dataFound) === 1){
+                if($this->password === $dataFound['row1']['passCode']){
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public function __destruct(){
--- src/features/signin.php
+++ src/features/signin.php
@@ -14,7 +14,7 @@ if (isset($_POST['send'])) {
 
 
     //DB info
-    $tableName = "player";
+    //$tableName = "player";
 
     //Load the content of the user-defined functions used to interact with MySQL
     include_once "../../db/Database.php";
@@ -22,17 +22,29 @@ if (isset($_POST['send'])) {
     include_once "../../db/Insert.php";
     include_once "../../db/Select.php";
   
-
     //Instanciate an object of the Create class used to create the database and table  
     //Create the database and tables
     $obj = new Create ();
 
     //Instanciate an object of Select Class to look for the user inside the database
-    $obj = new Select ($tableName, $user, $password);
-    $userLogged = $obj ->checkUser();
-    echo "the user logged is ". $userLogged->getUserName();
+    $obj = new Select ($user, $password);
 
+    $verifyUserName = $obj ->checkUserName();
+    if($verifyUserName === true){
+        echo "user found";
+    }
+    else {
+        echo "user name doesnt have an account";
+    }
 
+    $verifyPassword = $obj ->checkPassword();
+    if($verifyPassword === true){
+        echo "password is correct";
+    }
+    else {
+        echo "password not correct";
+    }
+    
 
 }
                     
