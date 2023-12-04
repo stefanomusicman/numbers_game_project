51016fbd59fba763f80a2079ea3694512f38a2b0
Stefano <stef.pro@hotmail.com>
2023-12-04 10:55:06 -0500

    modify ajax message for password on signup form

--- src/signup-onkeyup/pcode1-ajax.php
+++ src/signup-onkeyup/pcode1-ajax.php
@@ -5,5 +5,5 @@ $pcode1 = $_GET["pcode1"];
 if(strlen($pcode1) == 0) {
     echo "Field cannot be empty!";
 } else if(strlen($pcode1) < 8) {
-    echo "Username must be at least 8 characters";
+    echo "Password must be at least 8 characters";
 }
--- src/signup-onkeyup/pcode2-ajax.php
+++ src/signup-onkeyup/pcode2-ajax.php
@@ -5,5 +5,5 @@ $pcode2 = $_GET["pcode2"];
 if(strlen($pcode2) == 0) {
     echo "Field cannot be empty!";
 } else if(strlen($pcode2) < 8) {
-    echo "Username must be at least 8 characters";
+    echo "Password must be at least 8 characters";
 }
